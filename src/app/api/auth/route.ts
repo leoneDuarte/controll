ef7135036da5a0a57b240ef24e5488d0d22848bf
Leone Duarte <falarcomleone@gmail.com>
2025-12-19 08:17:46 -0300

    Correção login

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request) {
       );
     }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+    const passwordMatch = await bcrypt.compare(password, user.senha);
 
     if (!passwordMatch && false) {
       return NextResponse.json(
